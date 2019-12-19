cff826c7186e33403796ef854efbf4ee812093b0
elham <elham.e.khoda@cern.ch>
2019-12-18 18:05:03 -0800

    Updating EXOT7 data list

--- python/Data_EXOT7_rel21.py
+++ python/Data_EXOT7_rel21.py
@@ -1,49 +1,49 @@
 import TopExamples.grid
 
 TopExamples.grid.Add("Data15_13TeV_25ns_EXOT7").datasets = [
-'data15_13TeV.periodD.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3730',
-'data15_13TeV.periodE.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3730',
-'data15_13TeV.periodF.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3730',
-'data15_13TeV.periodG.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3730',
-'data15_13TeV.periodH.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3730',
-'data15_13TeV.periodJ.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3730',
+'data15_13TeV.periodD.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3841',
+'data15_13TeV.periodE.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3841',
+'data15_13TeV.periodF.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3841',
+'data15_13TeV.periodG.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3841',
+'data15_13TeV.periodH.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3841',
+'data15_13TeV.periodJ.physics_Main.PhysCont.DAOD_EXOT7.grp15_v01_p3841',
 ]
 
 TopExamples.grid.Add("Data16_13TeV_25ns_EXOT7").datasets = [
-'data16_13TeV.periodA.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
-'data16_13TeV.periodB.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
-'data16_13TeV.periodC.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
-'data16_13TeV.periodD.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
-'data16_13TeV.periodE.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
-'data16_13TeV.periodF.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
-'data16_13TeV.periodG.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
-'data16_13TeV.periodI.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
-'data16_13TeV.periodK.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
-'data16_13TeV.periodL.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3730',
+'data16_13TeV.periodA.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
+'data16_13TeV.periodB.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
+'data16_13TeV.periodC.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
+'data16_13TeV.periodD.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
+'data16_13TeV.periodE.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
+'data16_13TeV.periodF.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
+'data16_13TeV.periodG.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
+'data16_13TeV.periodI.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
+'data16_13TeV.periodK.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
+'data16_13TeV.periodL.physics_Main.PhysCont.DAOD_EXOT7.grp16_v01_p3841',
 ]
 
 # GRL: GoodRunsLists/data17_13TeV/20180618/data17_13TeV.periodAllYear_DetStatus-v97-pro21-17_Unknown_PHYS_StandardGRL_All_Good_25ns_Triggerno17e33prim.xml
 TopExamples.grid.Add("Data17_13TeV_25ns_EXOT7").datasets = [
-'data17_13TeV.periodB.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3730',
-'data17_13TeV.periodC.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3730',
-'data17_13TeV.periodD.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3730',
-'data17_13TeV.periodE.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3730',
-'data17_13TeV.periodF.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3730',
-'data17_13TeV.periodH.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3730',
-'data17_13TeV.periodI.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3730',
-'data17_13TeV.periodK.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3730',
+'data17_13TeV.periodB.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3841',
+'data17_13TeV.periodC.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3841',
+'data17_13TeV.periodD.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3841',
+'data17_13TeV.periodE.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3841',
+'data17_13TeV.periodF.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3841',
+'data17_13TeV.periodH.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3841',
+'data17_13TeV.periodI.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3841',
+'data17_13TeV.periodK.physics_Main.PhysCont.DAOD_EXOT7.grp17_v01_p3841',
 ]
 
 # GRL: GoodRunsLists/data18_13TeV/20190219/data18_13TeV.periodAllYear_DetStatus-v102-pro22-04_Unknown_PHYS_StandardGRL_All_Good_25ns_Triggerno17e33prim.xml
 TopExamples.grid.Add("Data18_13TeV_25ns_EXOT7").datasets = [
-'data18_13TeV.periodB.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
-'data18_13TeV.periodC.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
-'data18_13TeV.periodD.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
-'data18_13TeV.periodF.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
-'data18_13TeV.periodI.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
-'data18_13TeV.periodK.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
-'data18_13TeV.periodL.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
-'data18_13TeV.periodM.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
-'data18_13TeV.periodO.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
-'data18_13TeV.periodQ.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3730',
+'data18_13TeV.periodB.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
+'data18_13TeV.periodC.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
+'data18_13TeV.periodD.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
+'data18_13TeV.periodF.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
+'data18_13TeV.periodI.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
+'data18_13TeV.periodK.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
+'data18_13TeV.periodL.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
+'data18_13TeV.periodM.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
+'data18_13TeV.periodO.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
+'data18_13TeV.periodQ.physics_Main.PhysCont.DAOD_EXOT7.grp18_v01_p3841',
 ]
